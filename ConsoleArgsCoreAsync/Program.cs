d3e5dac15b1fabdbd976fb04cfaccff265268c39
Jeffery Cosby <jcoz1968@hotmail.com>
2018-08-25 19:41:12 -0500

    initial commit

--- ConsoleArgsCoreAsync/Program.cs
+++ ConsoleArgsCoreAsync/Program.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using McMaster.Extensions.CommandLineUtils;
+using System;
+using System.Threading.Tasks;
 
 namespace ConsoleArgsCoreAsync
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var app = new CommandLineApplication();
+
+            app.HelpOption("-h|--help");
+            var optionSubject = app.Option("-s|--subject <SUBJECT>", "The subject", CommandOptionType.SingleValue);
+            var optionRepeat = app.Option<int>("-n|--count <N>", "Repeat", CommandOptionType.SingleValue);
+
+            app.OnExecute(async () =>
+            {
+                var subject = optionSubject.HasValue()
+                    ? optionSubject.Value()
+                    : "world";
+
+                var count = optionRepeat.HasValue() ? optionRepeat.ParsedValue : 1;
+                for (var i = 0; i < count; i++)
+                {
+                    Console.Write($"Hello");
+
+                    // This pause here is just for indication that some awaitable operation could happens here.
+                    await Task.Delay(5000);
+                    Console.WriteLine($" {subject}!");
+                }
+            });
+
+            return app.Execute(args);
         }
     }
 }
